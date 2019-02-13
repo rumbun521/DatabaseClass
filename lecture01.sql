a4be3692cf589923e601eae84a20d78a99a43530
lab <mike.lab@Tnstate.edu>
2019-02-13 14:10:10 -0600

    updated script

--- lecture01.sql
+++ lecture01.sql
@@ -1,5 +1,3 @@
-USE employees;
-
-SHOW Tables;
-SELECT * FROM departments;
-SELECT * WHERE 1 = 1;
+#SELECT COUNT(DISTINCT emp_No) AS 'EmployeeCount' FROM salaries WHERE salary <= 50000 OR salary >= 60000;
+#SELECT COUNT(emp_No) AS 'EngineerCount' FROM titles WHERE title NOT IN ('engineer', 'senior engineer');
+SELECT SUM(salary) From salaries ORDER BY salary, from_date DESC;
